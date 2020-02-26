a8e6a8053e3b609fa615104f38310013ecf517d1
Denis Sipakov <mc.flyer@gmail.com>
2020-02-26 16:31:15 +0300

    Update URL

--- gallery/gallery/urls.py
+++ gallery/gallery/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path
+from django.views.static import serve
+
+from gallery import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
+    ]
