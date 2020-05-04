bafd7dd8902493125ff74b5062bf356cfc8f0cb4
Denis Sipakov <mc.flyer@gmail.com>
2020-05-04 19:55:28 +0300

    Image search by uuid

--- gallery/images/urls.py
+++ gallery/images/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from images import views
 
 urlpatterns = [
-    url(r'^images/?$', views.ImageListView.as_view(), name='image_list')
+    url(r'^images/?$', views.ImageListView.as_view(), name='image_list'),
+    url(r'^image/(?P<uuid>[a-zA-Z0-9]+)', views.ImageView.as_view(), name='image')
 ]
--- gallery/images/views.py
+++ gallery/images/views.py
@@ -1,4 +1,5 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
 
 from images.models import Image
 from images.serializers import ImageSerializer
@@ -9,3 +10,13 @@ class ImageListView(ListAPIView):
 
     def get_queryset(self):
         return Image.objects.all().order_by("date_created").reverse()[:5]
+
+
+class ImageView(RetrieveAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+    lookup_field = "uuid"
+
+    def retrieve(self, request, *args, **kwargs):
+        image = self.get_object()
+        return Response(self.get_serializer(image).data)
