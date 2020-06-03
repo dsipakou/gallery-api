74b7350a8a0fdc8e3bb8c304f23bdae4037d2fa5
Denis Sipakov <mc.flyer@gmail.com>
2020-06-03 19:41:17 +0300

    Show date in admin

--- gallery/images/admin.py
+++ gallery/images/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from images.models import Image, Like
 
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('admin_image_tag', 'name', 'date_created', 'date_updated')
+    list_display = ('admin_image_tag', 'name', 'date', 'date_created', 'date_updated')
     readonly_fields = ['uuid']
 
 admin.site.register(Image, ImageAdmin)
