de962fd30c78be6ce1352d97533cf6d0049de9a2
Denis Sipakov <mc.flyer@gmail.com>
2020-09-15 21:00:31 +0300

    Add custom s3 backend

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -42,7 +42,7 @@ AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 AWS_DEFAULT_ACL = None
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+DEFAULT_FILE_STORAGE = "images.utils.CustomS3Boto3Storage"
 
 # Application definition
 
